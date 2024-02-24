069a632f88f1f533881cbc2221910a1cdb4f8121
Ravi Singh <ravi149185@gmail.com>
2024-02-24 16:57:02 +0530

    Add tailwind-merge and react-icons dependencies

--- src/App.js
+++ src/App.js
@@ -1,21 +1,99 @@
 import Button from "./components/Button";
+import { FaArrowAltCircleRight } from "react-icons/fa";
 
 function App() {
   return (
-    <div>
-      <Button secondary>success</Button>
-      <Button primary rounded>
-        primary
-      </Button>
-      <Button secondary outline>
-        secondary
-      </Button>
-      <Button warning outline>
-        warning
-      </Button>
-      <Button danger outline>
-        danger
-      </Button>
+    <div className="flex flex-row">
+      <div className="m-5 ">
+        <Button success rounded outline className="mb-5">
+          <FaArrowAltCircleRight />
+          success
+        </Button>
+        <Button success rounded className="mb-5">
+          <FaArrowAltCircleRight />
+          success
+        </Button>
+        <Button success outline className="mb-5">
+          <FaArrowAltCircleRight />
+          success
+        </Button>
+        <Button success className="mb-5">
+          <FaArrowAltCircleRight />
+          success
+        </Button>
+      </div>
+      <div className="m-5">
+        <Button primary rounded outline className="mb-5">
+          <FaArrowAltCircleRight />
+          primary
+        </Button>
+        <Button primary rounded className="mb-5">
+          <FaArrowAltCircleRight />
+          primary
+        </Button>
+        <Button primary outline className="mb-5">
+          <FaArrowAltCircleRight />
+          primary
+        </Button>
+        <Button primary className="mb-5">
+          <FaArrowAltCircleRight />
+          primary
+        </Button>
+      </div>
+      <div className=" m-5">
+        <Button danger rounded outline className="mb-5">
+          <FaArrowAltCircleRight />
+          danger
+        </Button>
+        <Button danger rounded className="mb-5">
+          <FaArrowAltCircleRight />
+          danger
+        </Button>
+        <Button danger outline className="mb-5">
+          <FaArrowAltCircleRight />
+          danger
+        </Button>
+        <Button danger className="mb-5">
+          <FaArrowAltCircleRight />
+          danger
+        </Button>
+      </div>
+      <div className=" m-5">
+        <Button warning rounded outline className="mb-5">
+          <FaArrowAltCircleRight />
+          warning
+        </Button>
+        <Button warning rounded className="mb-5">
+          <FaArrowAltCircleRight />
+          warning
+        </Button>
+        <Button warning outline className="mb-5">
+          <FaArrowAltCircleRight />
+          warning
+        </Button>
+        <Button warning className="mb-5">
+          <FaArrowAltCircleRight />
+          warning
+        </Button>
+      </div>
+      <div className=" m-5">
+        <Button secondary rounded outline className="mb-5">
+          <FaArrowAltCircleRight />
+          secondary
+        </Button>
+        <Button secondary rounded className="mb-5">
+          <FaArrowAltCircleRight />
+          secondary
+        </Button>
+        <Button secondary outline className="mb-5">
+          <FaArrowAltCircleRight />
+          secondary
+        </Button>
+        <Button secondary className="mb-5">
+          <FaArrowAltCircleRight />
+          secondary
+        </Button>
+      </div>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,6 @@
 import React from "react";
 import className from "classnames";
+import { twMerge } from "tailwind-merge";
 
 const Button = ({
   children,
@@ -10,24 +11,30 @@ const Button = ({
   danger,
   outline,
   rounded,
+  ...rest
 }) => {
-  const classes = className("px-3 py-1.5 border", {
-    "bg-blue-500 border-blue-500 text-white": primary,
-    "bg-gray-500 border-gray-500 text-white": secondary,
-    "bg-green-500 border-green-500 text-white": success,
-    "bg-yellow-500 border-yellow-500 text-white": warning,
-    "bg-red-500 border-red-500 text-white": danger,
-    "rounded-full": rounded,
-    "bg-white": outline,
-    "text-blue-500": outline && primary,
-    "text-gray-500": outline && secondary,
-    "text-green-500": outline && success,
-    "text-yellow-500": outline && warning,
-    "text-red-500": outline && danger,
-  });
+  const classes = twMerge(
+    className(rest.className, "flex items-center px-3 py-1.5 border", {
+      "border-blue-500 bg-blue-500 text-white": primary,
+      "border-gray-900 bg-gray-900 text-white": secondary,
+      "border-green-500 bg-green-500 text-white": success,
+      "border-yellow-400 bg-yellow-400 text-white": warning,
+      "border-red-500 bg-red-500 text-white": danger,
+      "rounded-full": rounded,
+      "bg-white": outline,
+      "text-blue-500": outline && primary,
+      "text-gray-900": outline && secondary,
+      "text-green-500": outline && success,
+      "text-yellow-400": outline && warning,
+      "text-red-500": outline && danger,
+    })
+  );
+
   return (
     <div>
-      <button className={classes}>{children}</button>
+      <button {...rest} className={classes}>
+        {children}
+      </button>
     </div>
   );
 };
